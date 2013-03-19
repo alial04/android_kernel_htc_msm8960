283b61d1d0a1cdad2f619e7c25775294483a4441
Ajay Singh Parmar <aparmar@codeaurora.org>
2013-03-19 13:00:57 +0530

    msm_fb: hdmi: Remove supported resolution check
    The change will remove the supported resolution check while turning
    on the HDMI. The supported resolution is updated on parsing EDID for
    VIC and matching it with default mode in HDMI. This may fail in case
    the default video mode is not available in EDID of TV resulting in
    HDMI turn on failure.
    
    Also, this check is not needed because we now have the HDMI display
    pipe line check which will make sure we do not turn on HDMI if it
    is not supposed to be turned ON.
    
    CRs-Fixed: 461486
    Change-Id: I09ba863cb6f56c6c5d599b91516855f0540f17c3
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

--- drivers/video/msm/external_common.h
+++ drivers/video/msm/external_common.h
@@ -68,7 +68,6 @@ struct external_common_state_type {
 	boolean hpd_state;
 	struct kobject *uevent_kobj;
 	uint32 video_resolution;
-	boolean default_res_supported;
 	bool vcdb_support;
 	struct device *dev;
 	struct switch_dev sdev;
